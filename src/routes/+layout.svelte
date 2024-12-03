8415c8899ac9dce0ab2f499047c358d3386fb1a2
Clément Voisin <clembs@clembs.com>
2024-12-03 12:34:49 +0100

    feat: initialize phosphor icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,21 @@
 
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { IconContext } from 'phosphor-svelte';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	<div class="content">
-		{@render children()}
-	</div>
+	<IconContext
+		values={{
+			size: 24,
+			weight: 'duotone'
+		}}
+	>
+		<div class="viewport">
+			<main>
+				{@render children()}
+			</main>
+		</div>
+	</IconContext>
 </ParaglideJS>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltePhosphorOptimize } from 'phosphor-svelte/vite';
 
 export default defineConfig({
 	plugins: [
+		sveltePhosphorOptimize(),
 		sveltekit(),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
 		})
-	]
+	],
+	optimizeDeps: {
+		exclude: ['phosphor-svelte']
+	}
 });
