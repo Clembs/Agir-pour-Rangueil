48464b8bb08881beea9949ceaf1c03ad1b5f2b50
Clément Voisin <clembs@clembs.com>
2024-12-03 17:26:55 +0100

    fix: return to phosphor-svelte v2

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 // @ts-check
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { phosphorSvelteOptimize } from 'phosphor-svelte/preprocessor';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess()],
+	preprocess: [phosphorSvelteOptimize(), vitePreprocess()],
 
 	kit: {
 		adapter: adapter()
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { sveltePhosphorOptimize } from 'phosphor-svelte/vite';
 
 export default defineConfig({
 	plugins: [
-		sveltePhosphorOptimize(),
 		sveltekit(),
 		paraglide({
 			project: './project.inlang',
