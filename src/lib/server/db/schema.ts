02837b1dc98b67d744bd2794285ef057ee4f590b
Clément Voisin <clembs@clembs.com>
2024-12-03 12:04:06 +0100

    initial styling and schema wiring

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,4 +1,4 @@
-import { mysqlTable, serial, text, int } from 'drizzle-orm/mysql-core';
+import { mysqlTable, serial, int } from 'drizzle-orm/mysql-core';
 
 export const user = mysqlTable('user', {
 	id: serial('id').primaryKey(),
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+	import '../app.scss';
+
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>
-	{@render children()}
+	<div class="content">
+		{@render children()}
+	</div>
 </ParaglideJS>
