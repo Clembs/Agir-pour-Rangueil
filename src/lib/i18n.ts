efd366900f8b8c275e3d9c82dbcff2633bc635bc
Clément Voisin <clembs@clembs.com>
2024-12-03 17:30:41 +0100

    feat: i18n language store + relative time formatter

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,4 +1,12 @@
+import { page } from '$app/stores';
 import * as runtime from '$lib/paraglide/runtime';
 import { createI18n } from '@inlang/paraglide-sveltekit';
+import { get, readable } from 'svelte/store';
 
 export const i18n = createI18n(runtime);
+
+export const language = readable('en', (set) => {
+	set(i18n.getLanguageFromUrl(get(page).url));
+
+	return () => {};
+});
