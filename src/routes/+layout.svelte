d4b0c91872930f791359405f7486289833bcb336
Clément Voisin <clembs@clembs.com>
2024-12-03 17:29:50 +0100

    chore: cleanup unused code/files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import '../app.scss';
-
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	import NavBar from './NavBar.svelte';
 	import { IconContext } from 'phosphor-svelte';
+
 	let { children } = $props();
 </script>
 
@@ -16,9 +16,7 @@
 		}}
 	>
 		<div class="viewport">
-			<main>
-				{@render children()}
-			</main>
+			{@render children()}
 
 			<NavBar />
 		</div>
