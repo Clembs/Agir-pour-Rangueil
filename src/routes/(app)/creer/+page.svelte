7f3eb3cd472279dd44acda3f9dc17ab6406318fc
Clément Voisin <clembs@clembs.com>
2024-12-08 18:38:12 +0100

    feat: camera component with controls (wip)

---
+++ src/routes/(app)/creer/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Camera from './Camera.svelte';
+</script>
+
+<main>
+	<Camera />
+</main>
+
+<style lang="scss">
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+		gap: 1rem;
+		min-height: 100vh;
+		min-height: 100dvh;
+	}
+</style>
---
+++ src/routes/(app)/creer/Camera.svelte
@@ -0,0 +1,233 @@
+<script lang="ts">
+	import Skeleton from '$components/Skeleton.svelte';
+	import { ArrowsCounterClockwise, Camera, CameraSlash, ImagesSquare } from 'phosphor-svelte';
+	import { onMount } from 'svelte';
+
+	let videoState = $state<'ready' | 'loading' | 'error'>('loading');
+	let wrapperEl = $state<HTMLDivElement>();
+	let videoEl = $state<HTMLVideoElement>();
+	let currentTrack = $state<MediaStreamTrack>();
+	let videoDevices = $state<MediaDeviceInfo[]>([]);
+
+	onMount(async () => {
+		if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+			try {
+				// get the first camera
+				const stream = await navigator.mediaDevices.getUserMedia({
+					video: true
+				});
+
+				if (stream.active && videoEl) {
+					videoEl.srcObject = stream;
+					currentTrack = stream.getVideoTracks()[0];
+					videoState = 'ready';
+
+					// check if there's more than one camera
+					videoDevices = (await navigator.mediaDevices.enumerateDevices()).filter(
+						(device) => device.kind === 'videoinput'
+					);
+				}
+			} catch (error) {
+				videoState = 'error';
+			}
+		} else {
+			videoState = 'error';
+		}
+	});
+
+	function capture() {
+		if (!videoEl || videoState === 'error' || !currentTrack || !wrapperEl) return;
+
+		videoEl.pause();
+		wrapperEl.animate([{ scale: 1 }, { scale: 0.8 }, { scale: 1 }], { duration: 250 });
+
+		const canvas = document.createElement('canvas');
+		canvas.width = videoEl.videoWidth;
+		canvas.height = videoEl.videoHeight;
+
+		const context = canvas.getContext('2d');
+		context?.drawImage(videoEl, 0, 0, canvas.width, canvas.height);
+
+		const data = canvas.toDataURL('image/png');
+
+		// TODO: send to the server
+	}
+
+	async function switchCamera() {
+		if (
+			!videoEl ||
+			videoState === 'error' ||
+			!currentTrack ||
+			videoDevices.length === 1 ||
+			!wrapperEl
+		)
+			return;
+
+		// animate the arrows icon
+		const arrows = document.getElementById('arrows');
+		arrows?.animate([{ rotate: '0deg' }, { rotate: '-180deg' }], { duration: 125 });
+
+		// get the camera facing the other way
+		const stream = await navigator.mediaDevices.getUserMedia({
+			video: {
+				facingMode: currentTrack.getSettings().facingMode === 'user' ? 'environment' : 'user'
+			}
+		});
+
+		if (stream.active) {
+			videoEl.pause();
+
+			const animation = wrapperEl.animate(
+				[{ transform: 'scaleX(1)' }, { transform: 'scaleX(0.2)' }],
+				{ duration: 125 }
+			);
+
+			await animation.finished;
+
+			// once the first half of the animation is done, switch the video stream
+			videoEl.srcObject = stream;
+			currentTrack = stream.getVideoTracks()[0];
+
+			// complete the animation
+			wrapperEl.animate([{ transform: 'scaleX(0.2)' }, { transform: 'scaleX(1)' }], {
+				duration: 125
+			});
+
+			videoEl.play();
+		}
+	}
+</script>
+
+<div id="camera-wrapper" bind:this={wrapperEl}>
+	{#if videoState === 'loading'}
+		<Skeleton height="100%" width="100%" rounded={false} />
+	{:else if videoState === 'error'}
+		<div id="error-icon">
+			<CameraSlash size={64} />
+		</div>
+	{/if}
+
+	<video ondblclick={switchCamera} disablePictureInPicture autoplay muted bind:this={videoEl}
+	></video>
+</div>
+
+{#if videoState === 'error'}
+	<div id="error-text">
+		<h1>Allow IUT Vert to access your camera</h1>
+
+		<p>
+			To post your ecological pictures, you must grant camera access. We only use it to show you a
+			preview of your pics before you post them, and to post them.
+		</p>
+	</div>
+{:else}
+	<div id="main-actions">
+		<form action="/api/post?/">
+			<label aria-disabled={videoState !== 'ready'} for="file-input" class="action">
+				<ImagesSquare weight="fill" />
+				<input disabled={videoState !== 'ready'} type="file" name="file-input" id="file-input" />
+			</label>
+		</form>
+
+		<button onclick={capture} disabled={videoState !== 'ready'} id="take-picture" class="action">
+			<Camera weight="fill" />
+		</button>
+
+		<button
+			onclick={switchCamera}
+			disabled={videoState !== 'ready' || videoDevices.length === 1}
+			class="action"
+		>
+			<ArrowsCounterClockwise id="arrows" weight="fill" />
+		</button>
+	</div>
+{/if}
+
+<style lang="scss">
+	#camera-wrapper {
+		aspect-ratio: 1;
+		height: auto;
+		width: 100%;
+		overflow: hidden;
+		border-radius: 1rem;
+
+		#error-icon {
+			display: grid;
+			place-items: center;
+			height: 100%;
+			width: 100%;
+			background-color: var(--color-surface);
+			color: var(--color-on-surface);
+		}
+
+		video {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+	}
+
+	#error-text {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		align-items: center;
+
+		h1 {
+			text-align: center;
+			text-wrap: balance;
+		}
+	}
+
+	#main-actions {
+		display: flex;
+		gap: 1rem;
+
+		.action {
+			border: none;
+			background-color: var(--color-surface);
+			color: var(--color-on-surface);
+			padding: 1rem;
+			border-radius: 1rem;
+			display: grid;
+			place-items: center;
+			transition: transform 0.2s;
+
+			&:disabled,
+			&[aria-disabled='true'] {
+				opacity: 0.5;
+				cursor: not-allowed;
+			}
+
+			:global(#arrows) {
+				transition: transform 0.125s;
+			}
+
+			&:hover:active {
+				transform: scale(0.95);
+
+				// start the animation subtly
+				&:global(:has(#arrows) #arrows) {
+					transform: rotate(20deg);
+				}
+			}
+		}
+
+		#take-picture {
+			width: 100%;
+			flex-grow: 1;
+			background-color: var(--color-accent);
+			color: var(--color-on-accent);
+		}
+
+		form {
+			display: contents;
+
+			label {
+				input {
+					display: none;
+				}
+			}
+		}
+	}
+</style>
