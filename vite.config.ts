1a04866b6f8768a7c79f508ab7971f1b80b60be8
Clément Voisin <clembs@clembs.com>
2025-01-20 11:40:27 +0100

    fix: remove certificate based development

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'node:fs';
-import path from 'node:path';
-
-if (!process.env.KEY_PATH || !process.env.CERTIFICATE_PATH) {
-	console.warn(
-		"No KEY_PATH or CERTIFICATE_PATH environment variables found. HTTPS development won't work without them."
-	);
-} else {
-	console.log('Using HTTPS development.');
-}
 
 export default defineConfig({
 	plugins: [
@@ -20,17 +10,6 @@ export default defineConfig({
 			outdir: './src/lib/paraglide'
 		})
 	],
-	...(process.env.KEY_PATH && process.env.CERTIFICATE_PATH
-		? {
-				server: {
-					https: {
-						key: fs.readFileSync(path.resolve(__dirname, process.env.KEY_PATH)),
-						cert: fs.readFileSync(path.resolve(__dirname, process.env.CERTIFICATE_PATH))
-					},
-					host: '0.0.0.0'
-				}
-			}
-		: {}),
 	optimizeDeps: {
 		exclude: ['phosphor-svelte']
 	}
